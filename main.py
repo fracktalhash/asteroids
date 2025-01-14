e504b0b23dd07d9b79047d6a568046da40f90361
Fractal <fracktalhash@gmail.com>
2025-01-13 20:12:32 -0500

    Feat: Add player object and hit box
        - Create circle shape and player modules
        - Integrate hit box into player logic
        - Integrate draw player in main game loop

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +15,10 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen) #draw player
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000 #frame rate 60fps
 
 if __name__ == "__main__":
     main()
