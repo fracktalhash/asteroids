9465c1113f50ea4e09847a232e6182778dc0646d
Fractal <fracktalhash@gmail.com>
2025-01-13 21:03:50 -0500

    Feat: Add player motion and draw and update groups

--- main.py
+++ main.py
@@ -7,15 +7,25 @@ def main():
     screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
     clock = pygame.time.Clock()
     dt = 0
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            
+        for updatable in updatables:
+            updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen) #draw player
+
+        for drawable in drawables:
+            drawable.draw(screen) #draw player
+            
         pygame.display.flip()
 
         dt = clock.tick(60)/1000 #frame rate 60fps
