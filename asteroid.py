95d05dc4aacf8b5d7097dfc33fd132aa5dc6bf5b
Fractal <fracktalhash@gmail.com>
2025-01-15 12:30:15 -0500

    Feat: Implemented collision logic for bullets and asteroids with .split() for separation

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
+import random
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        
+        random_angle = random.uniform(20, 50)
+        new_vector1 = self.velocity.rotate(random_angle)
+        new_vector2 = self.velocity.rotate(-random_angle)
+        offset = self.radius*.55
+        pos1 = self.position + (new_vector1.normalize() * offset)
+        pos2 = self.position + (new_vector2.normalize() * offset)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(pos1.x,pos1.y, new_radius)
+        asteroid2 = Asteroid(pos2.x,pos2.y, new_radius)
+        asteroid1.velocity = new_vector1 * 1.2
+        asteroid2.velocity = new_vector2 * 1.2
